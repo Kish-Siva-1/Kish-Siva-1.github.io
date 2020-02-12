4fc7e71d9cd95ff1436121a990a9e15894b10861
Kish-Siva-1 <rodes88@msn.com>
2020-02-12 16:33:36 +0100

    Updates

---
+++ precache-manifest.6792ea36ca976c42d51cf93a2ca3ac7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921d3dd7798cd871e7f5eba2fc5cd42c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00b7bc65199f1a933d35",
+    "url": "/static/css/main.fca1e99b.chunk.css"
+  },
+  {
+    "revision": "b7cbfa5cc8a02aadc17b",
+    "url": "/static/js/2.3e2b5d3e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3e2b5d3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "00b7bc65199f1a933d35",
+    "url": "/static/js/main.22968950.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
